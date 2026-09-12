9a4d30a8f7711857b41467b0b655a6b6dc2c8485
dinraj910 <dinrajkdinesh564@gmail.com>
2026-09-12 16:23:48 +0530

    feat: add Babel configuration with Expo, NativeWind, and Reanimated

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -2,8 +2,8 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind", worklets: false }],
-      "nativewind/babel",
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
     ],
+    plugins: ["react-native-reanimated/plugin"],
   };
 };
